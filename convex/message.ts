3f6230ce292202e008911afa0e4bed2700acba44
Daniele Di Pinto <danieledipinto.c@gmail.com>
2024-08-04 14:43:37 +0200

    lesgoski

--- convex/message.ts
+++ convex/message.ts
@@ -1,5 +1,6 @@
 import { v } from "convex/values";
-import { query, mutation } from "./_generated/server";
+import { query, mutation, internalAction } from "./_generated/server";
+import { internal } from "./_generated/api";
 
 export const get = query({
   args: {},
@@ -17,5 +18,36 @@ export const post = mutation({
     await ctx.db.insert("messages", {
       ...args,
     });
+
+    await ctx.scheduler.runAfter(0, internal.message.sendNotifications, {
+      message: args.text,
+    });
+  },
+});
+
+export const sendNotifications = internalAction({
+  args: { message: v.string() },
+  handler: async (_, args): Promise<void> => {
+    try {
+      // A fetch request to send data through the discord
+      // webhook, and display it as a message in your
+      // discord channel
+      await fetch(
+        "https://discord.com/api/webhooks/1269627007247847485/TDgJ8JEFzLGGfKGpebVNr-FkaJ70Af_27pTaEJRQOGu4_iQbp_ox-5ljWU3DjvMLkfEU",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            content:
+              "ORCODEO NUOVI MESSAGGI SULLA [CHAT!](https://chat-bolla.vercel.app/chat)",
+          }),
+        }
+      );
+    } catch (err: any) {
+      // Just in case :)
+      console.log(err.message);
+    }
   },
 });
